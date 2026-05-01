12619c955a9ecf89c9bbdc9a6af688d9e5ff69ea
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-05-01 18:32:37 -0300

    test(prisma): add unit tests for prisma on database service

--- test/unit/database/database.service.spec.ts
+++ test/unit/database/database.service.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { DatabaseService } from '../../../src/database/database.service';
+import { DatabaseService } from '@/database/database.service';
 
 describe('DatabaseService', () => {
-  let service: DatabaseService;
+  let prisma: DatabaseService;
+
+  const mockPrisma = {
+    $connect: jest.fn(),
+    $disconnect: jest.fn(),
+    onModuleInit: jest.fn(),
+    onModuleDestroy: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DatabaseService],
+      providers: [{ provide: DatabaseService, useValue: mockPrisma }],
     }).compile();
 
-    service = module.get<DatabaseService>(DatabaseService);
+    prisma = module.get<DatabaseService>(DatabaseService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('is defined', () => {
+    it('service should be defined', () => expect(prisma).toBeDefined());
+  });
+
+  describe('$connect', () => {
+    it('should call $connect', async () => {
+      jest.spyOn(prisma, '$connect').mockResolvedValue();
+      await prisma.$connect();
+      expect(prisma.$connect).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('$disconnect', () => {
+    it('should call $disconnect', async () => {
+      jest.spyOn(prisma, '$disconnect').mockResolvedValue();
+      await prisma.$disconnect();
+      expect(prisma.$disconnect).toHaveBeenCalledTimes(1);
+    });
   });
 });
