faabe89ce79d73b18f22266d427b7c75a4695788
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-05-06 18:03:50 -0300

    style(decorators): reorganize imports

--- backend/src/shared/decorators/get-user.decorator.ts
+++ backend/src/shared/decorators/get-user.decorator.ts
@@ -1,4 +1,6 @@
-import { createParamDecorator, type ExecutionContext } from '@nestjs/common';
+import { createParamDecorator } from '@nestjs/common';
+
+import type { ExecutionContext } from '@nestjs/common';
 
 export const GetUser = createParamDecorator(
   (data: string | undefined, ctx: ExecutionContext): any => {
