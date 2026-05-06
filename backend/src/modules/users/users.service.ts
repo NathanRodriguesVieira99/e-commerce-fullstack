d10cee323606022b5af37b079e4bdb3240f57994
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-05-06 19:25:24 -0300

    feat(users): implement getProfile method to retrieve user profile by ID

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { DatabaseService } from '@/database/database.service';
+
+import { UserResponseDto } from './dtos/user-response.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly db: DatabaseService) {}
+
+  async getProfile(userId: string): Promise<UserResponseDto> {
+    const profile = await this.db.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        email: true,
+        password: false,
+        role: true,
+        firstName: true,
+        lastName: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    if (!profile) throw new NotFoundException(`Resource not found`);
+
+    return profile;
+  }
+}
